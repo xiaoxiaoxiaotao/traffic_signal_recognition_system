59a0596ed966d15d3bff60cbe2db1ce80285db88
Daredemodaisuki <six6666666666666@yeah.net>
2024-04-13 00:36:07 +0800

    API @app.route("/pic_predict", methods=["GET", "POST"])

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return render_template("index.html", title="Hello")
+
+
+# 表单POST提交图片（暂定），name="pic"，返回待定
+@app.route("/pic_predict", methods=["GET", "POST"])
+def predict():
+    if request.method == "POST":
+        file = request.files["pic"]
+        print("file", file)
+        # TODO: predict
+        return 'OK'
+    else:
+        return render_template("pic_predict.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
