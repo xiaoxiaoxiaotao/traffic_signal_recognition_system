e87db5d2ac46df1d342c8ffb91882a8e93c9c83a
xiaoxiaoxiaotao <108865127+xiaoxiaoxiaotao@users.noreply.github.com>
2024-04-13 07:31:36 +0000

    edit the upload function and add a test post request 1. change the function of upload a picture 2. add a test post request to test the upload function

--- app.py
+++ app.py
@@ -1,22 +1,25 @@
 from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def root():
-    return render_template("index.html", title="Hello")
+    return render_template("index.html", title="Home")
 
-
-# 表单POST提交图片（暂定），name="pic"，返回待定
-@app.route("/pic_predict", methods=["GET", "POST"])
-def predict():
-    if request.method == "POST":
-        file = request.files["pic"]
-        print("file", file)
-        # TODO: predict
-        return 'OK'
-    else:
-        return render_template("pic_predict.html")
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'picture' not in request.files:
+        return 'No file part', 400
+    file = request.files['picture']
+    if file.filename == '':
+        return 'No selected file', 400
+    if file:
+        # filename = secure_filename(file.filename)
+        # file.save(os.path.join('uploads', filename))
+        # TODO: predict the image
+        return 'File uploaded successfully', 200
 
 
 if __name__ == "__main__":
