e48bca0b00050d6b2e4332b97bb1315875e66807
Daredemodaisuki <six6666666666666@yeah.net>
2024-04-13 14:53:15 +0800

    添加API响应 @app.route("/pic_predict/pass_info", methods=["POST"]) def before_predict() 在用户发送图片前调用，服务端收取并储存包含uid、rid（request_id）的表单，rid应为用户计算的图片特征值（hash或其他某种）
    TODO：服务端后续收到图片后，校验特征值并匹配rid

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 from flask import Flask, render_template, request
+import json
+import time
 
 app = Flask(__name__)
+user_request = {}
+
+REQUESTED = 0
+SUBMITTED = 1
+SUCCESSES = 2
+FAIL = -1
 
 @app.route("/")
 def hello_world():
@@ -13,10 +21,23 @@ def predict():
     if request.method == "POST":
         file = request.files["pic"]
         print("file", file)
+        # TODO：服务端后续收到图片后，校验特征值并匹配rid
         # TODO: predict
         return 'OK'
     else:
         return render_template("pic_predict.html")
+
+
+@app.route("/pic_predict/pass_info", methods=["POST"])
+def before_predict():
+    data = request.form
+    print(data)
+    user_id = data["uid"]
+    request_id = data["rid"]  # rid用户生成，采用图片hash
+    user_request.update({request_id: {"user": user_id, "statu": REQUESTED, "time": time.time(), "pic": None, "result": None}})
+    print(user_request)
+    result_json = json.dumps({"code": 200, "data": {"uid": user_id, "rid": request_id}})
+    return result_json
 
 
 if __name__ == "__main__":
